8c50bce171e5d3df7609bd8f6fb289735a721b99
wishyoudie <66038379+wishyoudie@users.noreply.github.com>
2022-05-07 20:12:17 +0300

    Implemented 'Add new repository' window, button and partially handler. TODO: Make handler work with model functions instead of simply writing to System.out

--- src/main/java/gitjet/Controller.java
+++ src/main/java/gitjet/Controller.java
@@ -1,23 +1,47 @@
 package gitjet;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
-
     @FXML
     private MenuItem analyzeNewMenuItem;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField newRepoField;
+
+    @FXML
+    private Button newRepoButton;
+
+    @FXML
+    protected void analyzeNewMenuItemClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("open-repo-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage getRepoStage = new Stage();
+        getRepoStage.setTitle("Analyze new repository");
+        getRepoStage.setScene(scene);
+        getRepoStage.initModality(Modality.APPLICATION_MODAL);
+        getRepoStage.show();
     }
 
     @FXML
-    protected void analyzeNewMenuItemClick() {
+    protected void newRepoFieldInsert() {
+        String newRepoTextUrl = newRepoField.getText();
 
+        System.out.println(newRepoTextUrl);
+        /*
+        check and pass to model
+        */
+        Stage stage = (Stage) newRepoField.getScene().getWindow();
+        stage.close();
     }
 }
