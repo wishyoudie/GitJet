5c4691caf7bef8eef4928536e0f940c320451249
s1ckoleg <74201332+s1ckoleg@users.noreply.github.com>
2022-05-08 19:21:07 +0300

    setter in CloneProjects added

--- src/main/java/gitjet/model/clonerepo/CloneProjects.java
+++ src/main/java/gitjet/model/clonerepo/CloneProjects.java
@@ -10,12 +10,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CloneProjects {
+    private String repo;
 
-    public static void main(String[] args) throws IOException, GitCloningException {
-        cloneProject(setUpLinks(new File("test.txt")));
-    }
+//    public static void main(String[] args) throws IOException, GitCloningException {
+//        cloneProject(setUpLinks(new File("test.txt")));
+//    }
 
-    public static List<String> setUpLinks(File file) throws IOException {
+    public static List<String> setUpLinks(File file) throws IOException { // for .txt file
         List<String> repos = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new FileReader(file));
 
@@ -27,23 +28,27 @@ public class CloneProjects {
         return repos;
     }
 
-    public static void cloneProject(List<String> repos) throws GitCloningException {
-        for (String repo: repos) {
-            File cloneDirectory = new File("clones/" + repo.replace("https://github.com/", ""));
-            cloneDirectory.mkdirs();
+    public void cloneProject() throws GitCloningException {
+//        for (String repo: repos) {
+        File cloneDirectory = new File("clones/" + repo.replace("https://github.com/", ""));
+        cloneDirectory.mkdirs();
 
-            try {
-                System.out.println("Cloning " + repo + " into " + cloneDirectory);
-                Git.cloneRepository()
-                        .setURI(repo)
-                        .setDirectory(Paths.get(String.valueOf(cloneDirectory)).toFile())
-                        .call();
-                System.out.println("Completed Cloning");
-            } catch (GitAPIException e) {
-                throw new GitCloningException(Errors.CLONE_ERROR.getMessage());
-            }
+        try {
+            System.out.println("Cloning " + repo + " into " + cloneDirectory);
+            Git.cloneRepository()
+                    .setURI(repo)
+                    .setDirectory(Paths.get(String.valueOf(cloneDirectory)).toFile())
+                    .call();
+            System.out.println("Completed Cloning");
+        } catch (GitAPIException e) {
+            throw new GitCloningException(Errors.CLONE_ERROR.getMessage());
         }
+//        }
+    }
 
-
+    public void setRepos(String repo) {
+        if (!repo.isEmpty()) {
+            this.repo = repo;
+        }
     }
 }
